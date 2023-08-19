2dd2ec15922c39e282b1abfc0ae62abf250d492f
abirtasrif <abirtasrif@gmail.com>
2023-08-19 23:55:44 +0600

    working with button variants

--- app/components/ui/Button.tsx
+++ app/components/ui/Button.tsx
@@ -1,4 +1,6 @@
 import { cva, VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
-export const buttonVariants = cva();
+export const buttonVariants = cva(
+  "inline-block text-center px-6 py-3 rounded-xl border font-medium active:scale-105 "
+);
---
+++ app/user/signin/page.tsx
@@ -0,0 +1,7 @@
+import type { Metadata } from "next";
+
+const SignInPage = () => {
+  return <div>SignInPage</div>;
+};
+
+export default SignInPage;

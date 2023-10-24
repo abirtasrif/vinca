61d728e805a39170e204f0aa24ac8a66287ccfd6
abirtasrif <abirtasrif@gmail.com>
2023-10-25 00:20:36 +0600

    basic slider implemented

--- app/(home)/_components/Slider.tsx
+++ app/(home)/_components/Slider.tsx
@@ -4,21 +4,21 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 
 const Slider = () => {
   return (
     <section className='h-[calc(100vh-5rem)]'>
       <Swiper
-        // install Swiper modules
-        modules={[Navigation, Pagination]}
-        spaceBetween={50}
-        slidesPerView={3}
-        navigation
+        modules={[Navigation, Pagination, Autoplay]}
+        navigation={true}
         pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log('slide change')}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        grabCursor={true}
       >
         <SwiperSlide>Slide 1</SwiperSlide>
         <SwiperSlide>Slide 2</SwiperSlide>

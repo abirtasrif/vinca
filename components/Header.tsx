e792e0b3e47ae1640b5081d8bd8c5f6d260741a9
abirtasrif <abirtasrif@gmail.com>
2023-10-29 21:47:16 +0600

    added Navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+'use client';
+
+import React from 'react';
+import { cn } from '@/libs/utils';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { buttonVariants } from './ui/Button';
+
+const navContents = [
+  { href: '/', label: 'Home' },
+  { href: '/beauty-packages', label: 'Beauty Packages' },
+  { href: '/specialists', label: 'Specialists' },
+  { href: '/about', label: 'About' },
+  { href: '/contact', label: 'Contact' },
+];
 
 const Header = () => {
+  const pathname = usePathname();
   return (
-    <header className='h-20'>Header</header>
-  )
-}
+    <header className='fixed left-0 right-0 top-0 z-[100] flex h-20 w-full items-center border-b border-gray bg-white/90 backdrop-blur-xl'>
+      <nav className='container flex items-center justify-between gap-5'>
+        <Link href='/' className='text-2xl font-semibold'>
+          vinca<span className='text-blue'>.</span>
+        </Link>
+        <ul className='flex items-center gap-5 text-lg'>
+          {navContents.map((item) => (
+            <li key={item.label}>
+              <Link
+                href={item.href}
+                className={cn(
+                  'link-item',
+                  pathname === item.href ? 'text-black' : 'text-black/50'
+                )}
+              >
+                {item.label}
+              </Link>
+            </li>
+          ))}
 
-export default Header
+          <Link
+            href='/sign-in'
+            className={cn(buttonVariants({ variant: 'secondary' }))}
+          >
+            Sign In
+          </Link>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;

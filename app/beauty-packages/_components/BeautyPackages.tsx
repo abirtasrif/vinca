f9a3ffd0cafcddf604b29cd1ea2a9235b1d61fb6
abirtasrif <abirtasrif@gmail.com>
2023-11-08 22:22:43 +0600

    added fetcher hook

--- app/beauty-packages/_components/BeautyPackages.tsx
+++ app/beauty-packages/_components/BeautyPackages.tsx
@@ -1,10 +1,23 @@
+'use client';
+
 import SectionTitle from '@/components/ui/SectionTitle';
+import useFetch from '@/hooks/useFetch';
 import React from 'react';
 
 const BeautyPackages = () => {
+  const {
+    data: beautyPackages,
+    isLoading,
+    error,
+  } = useFetch('/api/beauty_packages');
+
   return (
     <section className='section-padding container'>
       <SectionTitle title='Beauty Packages' />
+
+      <div className='grid grid-cols-1 gap-20 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'>
+        {/* Beauty Packages */}
+      </div>
     </section>
   );
 };

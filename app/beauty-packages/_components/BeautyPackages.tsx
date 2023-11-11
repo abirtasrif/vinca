5dacf1264dd9e330a8e7dc65b1a07a9c8342016b
Abir Tasrif <abirtasrif@gmail.com>
2023-11-11 19:51:53 +0600

    uploaded with bug

--- app/beauty-packages/_components/BeautyPackages.tsx
+++ app/beauty-packages/_components/BeautyPackages.tsx
@@ -2,9 +2,14 @@
 
 import SectionTitle from '@/components/ui/SectionTitle';
 import useFetch from '@/hooks/useFetch';
-import React from 'react';
+import BeautyPackageCard from './BeautyPackageCard';
+import Loading from '@/components/ui/Loading';
 
-const BeautyPackages = () => {
+interface BeautyPackagesProps {
+  native?: boolean;
+}
+
+const BeautyPackages: React.FC<BeautyPackagesProps> = ({ native }) => {
   const {
     data: beautyPackages,
     isLoading,
@@ -15,9 +20,16 @@ const BeautyPackages = () => {
     <section className='section-padding container'>
       <SectionTitle title='Beauty Packages' />
 
-      <div className='grid grid-cols-1 gap-20 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'>
-        {/* Beauty Packages */}
-      </div>
+      {isLoading && <Loading isLoading={isLoading} />}
+
+      {beautyPackages && (
+        <div className='grid grid-cols-1 gap-20 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4'>
+          {/* Beauty Packages */}
+          {beautyPackages.map((item) => (
+            <BeautyPackageCard key={item._id} item={item} />
+          ))}
+        </div>
+      )}
     </section>
   );
 };
